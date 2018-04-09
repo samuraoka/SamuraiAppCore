e12c7fd5efe796f9be792414ad4469a21897361b
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-04-09 21:32:21 +0900

    Implemented UI for the battles.

--- SamuraiAppCore.Wpf/BattlesWindow.xaml.cs
+++ SamuraiAppCore.Wpf/BattlesWindow.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace SamuraiAppCore.Wpf
 {
@@ -24,20 +14,75 @@ namespace SamuraiAppCore.Wpf
             InitializeComponent();
         }
 
-        //TODO
-
-        private void GotoSamurais_Click(object sender, RoutedEventArgs e)
-        {
-            var samuraisWindow = new MainWindow();
-            samuraisWindow.Show();
-            Close();
-        }
-
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            //TODO
             System.Windows.Data.CollectionViewSource battleViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("battleViewSource")));
             // Load data by setting the CollectionViewSource.Source property:
             // battleViewSource.Source = [generic data source]
+        }
+
+        private void battleListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //TODO
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO
+        }
+
+        #region AddSamuraiToBattle
+
+        private void samuraisInBattle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //TODO
+        }
+
+        private void samuraisInBattle_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            //TODO
+        }
+
+        private void samuraisInBattle_DragEnter(object sender, DragEventArgs e)
+        {
+            //TODO
+        }
+
+        private void samuraisInBattle_Drop(object sender, DragEventArgs e)
+        {
+            //TODO
+        }
+
+        #endregion AddSamuraiToBattle
+
+        #region RemoveSamuraiFromBattle
+
+        private void samuraisNotInBattle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //TODO
+        }
+
+        private void samuraisNotInBattle_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            //TODO
+        }
+
+        private void samuraisNotInBattle_DragEnter(object sender, DragEventArgs e)
+        {
+            //TODO
+        }
+
+        private void samuraisNotInBattle_Drop(object sender, DragEventArgs e)
+        {
+            //TODO
+        }
+
+        #endregion RemoveSamuraiFromBattle
+
+        private void nameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //TODO
         }
 
         private void startDateDatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
@@ -50,9 +95,31 @@ namespace SamuraiAppCore.Wpf
             //TODO
         }
 
-        private void battleListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void NoteSamuraiMove()
         {
             //TODO
+        }
+
+        private void GatherSelectedItemForMove(object sender, MouseEventArgs e)
+        {
+            //TODO
+        }
+
+        private static void IgnoreNonSamuraiItem(object sender, DragEventArgs e)
+        {
+            //TODO
+        }
+
+        private void NewButton_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO
+        }
+
+        private void GotoSamurais_Click(object sender, RoutedEventArgs e)
+        {
+            var samuraisWindow = new MainWindow();
+            samuraisWindow.Show();
+            Close();
         }
     }
 }
